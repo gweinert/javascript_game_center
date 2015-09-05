ec149203396455dabab9d13f7344b70246201957
Garrett <garrettweinert@gmail.com>
2015-09-05 11:49:18 -0700

    adding files

---
+++ js/snakeScript.js
@@ -0,0 +1,287 @@
+$(document).ready(function(){
+
+  controller.init();
+
+});
+
+var view = {
+
+  buildGrid: function(){
+
+    for(var i = 1; i <= 20*20; i++ ){
+      $('<div class="cell" id = "'+ i +'"></div>').appendTo('.game-grid');
+    }
+
+  },
+
+  placeHead: function() {
+    $('#190').addClass('head');
+  },
+
+  updateScore: function(){
+    $('#score').html(model.score);
+  },
+
+
+  keyListener: function() {
+    $(document).keydown(function(e) {
+      
+    switch(e.which) {
+        //if/elses inside cases disallow 180 degree turnarounds if //snake has body
+
+        //right
+        case 37:
+          controller.hasGameStarted();
+          if ( controller.vel[0] == 1 && controller.snakeDivs.length > 0){
+            controller.vel = [1, 0];
+          }
+          else {
+            controller.vel = [-1, 0];
+          }
+          break;
+
+        //down
+        case 38:
+          controller.hasGameStarted();
+          if ( controller.vel[1] == 1 && controller.snakeDivs.length > 0){
+            controller.vel = [0, 1];
+          }
+          else{
+            controller.vel = [0, -1];
+          }
+          break;
+
+        //left
+        case 39:
+          controller.hasGameStarted();
+          if ( controller.vel[0] == -1 && controller.snakeDivs.length > 0){
+            controller.vel = [-1, 0];
+          }
+          else{
+            controller.vel = [1, 0];
+          }
+        break;
+
+        //up
+        case 40:
+          controller.hasGameStarted();
+          if ( controller.vel[1] == -1 && controller.snakeDivs.length > 0){
+            controller.vel = [0, -1];
+          }
+          else{
+            controller.vel = [0, 1];
+          }
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    console.log(controller.vel);
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+  },
+
+
+  buildSnakeFood: function(){
+    
+    //if no food on grid already create random grid coord
+    if ($('.food').length === 0) {
+      randomGridId = this.getRandomInt(1, 400);
+      
+      //creates new grid coord if food is placed on snake body
+      while( randomGridId != controller.headPosition && controller.snakeDivs.indexOf(randomGridId) != -1){
+        randomGridId = this.getRandomInt(1, 400);
+      }
+      //adds food to game grid
+      $('#' + randomGridId).addClass('food');
+    }
+
+  },
+
+  getRandomInt: function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+};
+
+var model = {
+  score: 0
+};
+
+var controller = {
+
+  //array of snake body parts
+  snakeDivs: [],
+
+  //current interval speed
+  updateInterval: 400,
+
+  //var for interval functions in order to reset later
+  snakeInterval: 1,
+
+  //sets gamestate to false until arrow key is pressed
+  gameStart: false,
+
+  //gets coord of div with class head
+  headPosition: parseInt($('.head').attr("id")),
+
+  //sets direction of snake
+  vel: [0,0],
+
+  init: function() {
+
+    view.buildGrid();
+    view.placeHead();
+
+    //starts browser listener for arrow keypresses
+    view.keyListener();
+  },
+
+  hasGameStarted: function(){
+    if (!controller.gameStart) {
+      controller.playGame();
+      controller.gameStart = true;
+      console.log("startgin");
+    }
+  },
+
+  playGame: function() {
+    controller.snakeInterval = setInterval(function(){
+      
+      controller.updatePosition();
+
+    }, controller.updateInterval);
+
+  },
+
+  checkOutOfBounds: function(nextHeadPosition) {
+    // Out of bounds if true
+
+    switch(controller.vel.join()) {
+      case "1,0":
+        return (nextHeadPosition % 20 === 1 || isNaN(nextHeadPosition));
+        break;
+
+      case "-1,0":
+        return (nextHeadPosition % 20 === 0 || isNaN(nextHeadPosition));
+        break;
+
+      default:
+        return isNaN(nextHeadPosition);
+    }
+
+  },
+
+  //if next head position is not included in snakebody array
+  // or if next head position is not a boundary 
+  //game is not over
+  checkIfGameOver: function(nextHeadPosition) {
+
+    if ( (controller.snakeDivs.indexOf(nextHeadPosition) < 0 ) && !controller.checkOutOfBounds(nextHeadPosition) ) {
+      return false;
+    }
+    else {
+      console.log(nextHeadPosition)
+      return true;
+    }
+  },
+
+  resetGame: function(){
+    
+    clearInterval(controller.snakeInterval);
+    controller.updateInterval = 400;
+
+    //sets gamestate to false so player can start with keypress
+    controller.gameStart = false;
+    
+    //removes all snake body parts from game grid and game
+    $('div').removeClass('snake-body');
+    controller.snakeDivs = [];
+
+    //resets snake head to middle
+    view.placeHead();
+    
+    //resets score to zero
+    model.score = 0;
+    view.updateScore();
+  },
+
+  updatePosition: function() {
+
+    // Get position of head
+    controller.headPosition = parseInt($('.head').attr("id"));
+
+    // Add old head position to front of snakebody
+    controller.snakeDivs.unshift(controller.headPosition);
+
+    // Switch old head to snake body
+    $('.head').addClass('snake-body').removeClass('head');
+
+    // Update head position depending on last direction
+    nextHeadPosition = controller.headPosition;
+    nextHeadPosition += controller.vel[0];
+    nextHeadPosition += (controller.vel[1] * 20);
+
+    //checks if snake hits self or boundary
+    if (controller.checkIfGameOver(nextHeadPosition)) {
+      lastScore = model.score;
+      
+      controller.resetGame();
+
+      alert("Game Over with score " + lastScore);
+    }
+    else {
+
+      //moves head to the next position
+      $('#' + nextHeadPosition).addClass('head');
+
+      // // Remove tail from snakeDivs to move snake forward
+      tail = controller.snakeDivs.pop();
+      $('#' + tail).removeClass('snake-body');
+
+      // Check if position is snake food
+      controller.eatSnakeFood();
+
+      // Spawn snake food
+      view.buildSnakeFood();
+
+    }
+  },
+
+  resetInterval: function(){
+    
+    controller.updateInterval *= 0.95;
+
+    clearInterval(controller.snakeInterval);
+    
+    controller.snakeInterval = setInterval( controller.updatePosition, controller.updateInterval);
+  },
+
+
+  eatSnakeFood: function() {
+
+      //if head position is at same spot as food position
+    if (controller.headPosition === parseInt($('.food').attr("id"))) {
+
+      //updates snake body to be longer
+      controller.snakeDivs.push($('food').attr('id'));
+
+      //removes food from game grid
+      $('.food').removeClass('food');
+
+      //increases snake speed
+      controller.resetInterval();
+
+      //updates score in view
+      model.score += 1;
+      view.updateScore();
+      }
+
+    else {
+      // return false;
+    }
+
+  }
+
+};
+
---
+++ js/space_invaders.js
@@ -0,0 +1,423 @@
+// Player controls spaceship at bottom of screen
+// can move left or right pressing arrow keys
+// can shoot pressing spacebar
+// 5 rows of 11 minions spawn at top of screen
+// Grid is 53 x 20
+// move towards player traveling across the screen and then down
+
+$(document).ready(function(){
+
+  controller.init();
+
+});
+
+
+var view = {
+
+  //Generate grid
+  gridSize: [20, 20],
+
+  init: function() {
+    this.generateGrid();
+    this.spawnPlayer();
+    this.spawnEnemies();
+    this.spawnEnemyShooters();
+    this.keyListener();
+  },
+
+  generateGrid: function() {
+    for (var i = 1; i <= (this.gridSize[0] * this.gridSize[1]); i++) {
+      $('.game-grid').append("<div class='cell' id='" + i + "'></div>");
+    }
+  },
+
+  spawnPlayer: function() {
+    $('#390').addClass("player");
+    controller.currentPlayerPosition = 390;
+  },
+
+  spawnEnemies: function() {
+    for (var i = 0; i < controller.currentEnemies.length ; i ++ ) {
+      
+      $( '#' + controller.currentEnemies[i] ).addClass('enemy');
+
+    }
+  },
+
+  spawnEnemyShooters: function(){
+    for(var i = 0; i < controller.enemyShooters.length; i++ ){
+      $( '#' + controller.enemyShooters[i] ).addClass('shooter');
+    }
+  },
+
+  spawnBullet: function() {
+    $('#370').addClass("bullet");
+  },
+
+  updateEnemyViews: function(){
+    $('.enemy').removeClass('enemy');
+    for(var i = 0; i<controller.currentEnemies.length; i++){
+      $('#'+controller.currentEnemies[i]).addClass('enemy');
+    }
+  },
+
+  updateShooterViews: function(){
+    $('.shooter').removeClass('shooter');
+    for(var i = 0; i<controller.enemyShooters.length; i++){
+      $('#'+controller.enemyShooters[i]).addClass('shooter');
+    }
+
+  },
+
+  moveEnemiesNumberOfSpaces: function(numberOfSpaces){
+    for (var i = 0; i < controller.currentEnemies.length ; i ++ ) {
+      controller.currentEnemies[i] += numberOfSpaces;
+    }
+  },
+
+  moveShooterEnemiesNumberOfSpaces: function(numberOfSpaces){
+    for(var i = 0; i< controller.enemyShooters.length ; i++ ){
+      controller.enemyShooters[i] += numberOfSpaces;
+    }
+  },
+
+  moveEnemyPositions: function(){
+    if (controller.enemyCounter % 10 === 0 ){
+      view.moveEnemiesNumberOfSpaces(20);
+      view.moveShooterEnemiesNumberOfSpaces(20);
+      controller.enemyCounter = 0;
+      controller.enemyDirection *= -1;
+    }
+    else{
+      view.moveEnemiesNumberOfSpaces( controller.enemyDirection );
+      view.moveShooterEnemiesNumberOfSpaces( controller.enemyDirection);
+
+    }
+    controller.enemyCounter ++;
+  },
+
+
+
+  updatePlayerPosition: function() {
+    $('.player').removeClass('player');
+    $('#' + controller.currentPlayerPosition).addClass('player');
+
+  },
+
+  movePlayer: function(n) {
+    if (n == 1) {
+      if (controller.currentPlayerPosition != 400){
+        controller.currentPlayerPosition += 1;
+      }
+    }
+    else {
+      if (controller.currentPlayerPosition != 381) {
+        controller.currentPlayerPosition -= 1;
+      }
+    }
+    view.updatePlayerPosition();
+  },
+
+  moveBullet: function(i){
+
+    $('#' + i).removeClass('bullet');
+    $('#' + (i - 20)).addClass('bullet');
+
+    return i - 20;
+
+  },
+
+  moveEnemyBullet: function(i){
+    $('#' + i).removeClass('bullet');
+    $('#' + (i + 20)).addClass('bullet');
+    return i + 20;
+  },
+
+  keyListener: function() {
+    $(document).keydown(function(e) {
+      switch(e.which) {
+
+          //right
+          case 39:
+            view.movePlayer(1);
+            break;
+
+          //left
+          case 37:
+            view.movePlayer(2);
+            break;
+
+          //space
+          case 32:
+
+            d = new Date()
+            if (d.getTime() - controller.playerBulletDelay > 750) {
+
+              controller.playerShoot();
+              controller.playerBulletDelay = d.getTime();
+            }
+            break;
+
+          default: return; // exit this handler for other keys
+      }
+
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+
+  }
+
+};
+
+
+var controller = {
+
+  init: function() {
+    this.createCurrentEnemiesArray();
+    view.init();
+    // this.createCurrentEnemiesArray();
+    this.playGame();
+    // controller.enemyIntervalTimout();
+  },
+
+  currentPlayerPosition: 390,
+  enemyIntervalTime: 1000,
+  currentEnemies: [],
+  enemyCounter: 1,
+  enemyDirection: 1,
+  enemyShooters: [],
+  enemyBullets: [],
+  playerBulletPosition: 390,
+  playerBulletIntervalTime: 100,
+  playerBullets: [],
+  playerBulletDelay: new Date().getTime(),
+
+  playGame: function() {
+
+    //expiremental
+    //////////////
+    ////////////
+    // controller.gameLoop = setInterval(function(){
+    controller.enemyIntervalTimout();
+    controller.createEnemyBulletsTimeout();
+    controller.enemyBulletInterval();
+    controller.playerBulletIntervalTimeout();
+  },
+
+  enemyIntervalTimout: function(){
+
+        view.moveEnemyPositions();
+        view.updateEnemyViews();
+        view.updateShooterViews();
+        setTimeout(controller.enemyIntervalTimout, 1000);
+
+  },
+
+  createEnemyBulletsTimeout: function(){
+
+        var shooter = controller.enemyShooters[Math.floor(Math.random() * controller.enemyShooters.length)];
+        controller.enemyBullets.push(shooter);
+        console.log("bulet created!");
+        setTimeout(controller.createEnemyBulletsTimeout, 1000);
+
+  },
+
+  enemyBulletInterval: function(){
+
+        numOfBullets = controller.enemyBullets.length;
+
+        for(var i = 0; i<=numOfBullets; i++ ){
+          bullet = controller.enemyBullets[i];
+    
+          if(controller.playerBulletCollision(bullet)){
+            $('#'+bullet).removeClass('bullet');
+            $('player').removeClass('player');
+            controller.enemyBullets.splice(controller.enemyBullets.indexOf(bullet), 1);
+            alert("you lose");
+          }
+          else{
+            bullet = view.moveEnemyBullet(bullet);
+            controller.enemyBullets[i] = bullet;
+            
+          }
+        }
+
+        setTimeout(controller.enemyBulletInterval, 700);
+
+  },
+
+  playerBulletIntervalTimeout: function(){
+        new_bullets = [];
+        len = controller.playerBullets.length;
+        for (var i = 0; i < len; i++) {
+          bullet = controller.playerBullets.pop();
+          
+          
+          if(controller.enemyBulletCollision(bullet)){
+  
+            if (controller.enemyShooters.indexOf(bullet) > -1) {
+              controller.enemyShooters.splice(controller.enemyShooters.indexOf(bullet), 1);
+              if($('#'+(bullet-20)).hasClass('enemy')){
+                controller.enemyShooters.push(bullet-20);
+              }
+            }
+            
+            $('#' + bullet).removeClass('bullet');
+            
+            console.log("bullet_id: "+bullet);
+            
+            controller.currentEnemies.splice(controller.currentEnemies.indexOf(bullet), 1);
+            
+            controller.playerBullets.splice(controller.playerBullets.indexOf(bullet), 1);
+
+          }
+          else if( controller.enemyBullets.indexOf(bullet) > -1){
+            $('#'+bullet).removeClass('bullet');
+            controller.playerBullets.splice(controller.playerBullets.indexOf(bullet), 1);
+            controller.enemyBullets.splice(controller.enemyBullets.indexOf(bullet), 1);
+          }
+          else {
+            bullet = view.moveBullet(bullet);
+            new_bullets.push(bullet);
+          }
+        }
+        controller.playerBullets = new_bullets;
+      // }, controller.playerBulletIntervalTime);
+        setTimeout( controller.playerBulletIntervalTimeout, 100);
+
+    },
+
+    ////////////////
+    //expiremtnal
+    
+
+
+    // controller.enemyInterval = setInterval(function(){
+
+    //   view.moveEnemyPositions();
+    //   view.updateEnemyViews();
+    //   view.updateShooterViews();
+
+    // }, controller.enemyIntervalTime);
+
+    // controller.createEnemyBullets = setInterval(function(){
+
+    //   var shooter = controller.enemyShooters[Math.floor(Math.random() * controller.enemyShooters.length)];
+
+    //   controller.enemyBullets.push(shooter);
+
+    //   console.log("bulet created!");
+    // }, 5000);
+
+    // controller.enemyBulletInterval = setInterval(function(){
+
+    //   numOfBullets = controller.enemyBullets.length;
+
+    //   for(var i = 0; i<=numOfBullets; i++ ){
+    //     bullet = controller.enemyBullets[i];
+  
+    //     if(controller.playerBulletCollision(bullet)){
+    //       $('#'+bullet).removeClass('bullet');
+    //       $('player').removeClass('player');
+    //       controller.enemyBullets.splice(controller.enemyBullets.indexOf(bullet), 1);
+    //       alert("you lose");
+    //     }
+    //     else{
+    //       bullet = view.moveEnemyBullet(bullet);
+    //       controller.enemyBullets[i] = bullet;
+          
+    //     }
+    //   }
+    // }, 700);
+   
+
+    // controller.playerBulletInterval = setInterval(function(){
+
+    //   new_bullets = [];
+    //   len = controller.playerBullets.length;
+    //   for (var i = 0; i < len; i++) {
+    //     bullet = controller.playerBullets.pop();
+        
+        
+    //     if(controller.enemyBulletCollision(bullet)){
+    //       if (controller.enemyShooters.indexOf(bullet) > -1) {
+    //         controller.enemyShooters.splice(controller.enemyShooters.indexOf(bullet), 1);
+    //         if($('#'+(bullet-20)).hasClass('enemy')){
+    //           controller.enemyShooters.push(bullet-20);
+    //         }
+    //      }
+          
+    //       $('#' + bullet).removeClass('bullet');
+          
+    //       console.log("bullet_id: "+bullet);
+          
+    //       controller.currentEnemies.splice(controller.currentEnemies.indexOf(bullet), 1);
+          
+    //       controller.playerBullets.splice(controller.playerBullets.indexOf(bullet), 1);
+
+    //     }
+    //     else {
+    //       bullet = view.moveBullet(bullet);
+    //       new_bullets.push(bullet);
+    //     }
+    //   }
+    //   controller.playerBullets = new_bullets;
+    // }, controller.playerBulletIntervalTime);
+
+  // },
+
+  createCurrentEnemiesArray: function() {
+    for (var i = 40; i <= 160; i += 20) {
+      for (var j = 1; j <= 11; j++){
+        this.currentEnemies.push(i+j);
+        if( i > 160-12 ){
+          this.enemyShooters.push(i+j);
+        }
+      }
+    }
+  },
+
+  playerShoot: function(){
+
+    this.playerBullets.push( controller.currentPlayerPosition );
+
+  },
+
+  enemyShoot: function(){
+
+    var shooter = this.enemyShooters[Math.floor(Math.random() * this.enemyShooters.length)];
+    // console.log(shooter);
+  },
+
+  playerBulletCollision: function(bullet_id){
+    if( this.currentPlayerPosition === bullet_id ){
+      return true;
+    }
+    else{
+      return false;
+    }
+  },
+
+  enemyBulletCollision: function(bullet_id){
+    if( controller.currentEnemies.indexOf(bullet_id) > -1){
+      console.log("enemy hit");
+      return true;
+
+    }
+    else{
+      return false;
+    }
+  }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
